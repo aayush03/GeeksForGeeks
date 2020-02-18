2bdca9346c5ce62d507c0d5643a228032ebfa25c
Aayush Srivastava <aayush03@users.noreply.github.com>
2020-02-19 02:03:51 +0530

    RottenOranges

--- graphs/RottenOranges.java
+++ graphs/RottenOranges.java
@@ -8,6 +8,25 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * Given a matrix of dimension r*c where each cell in the matrix can have values 0, 1 or 2 which has the following meaning:
+ * 0 : Empty cell
+ * 1 : Cells have fresh oranges
+ * 2 : Cells have rotten oranges
+ *
+ * So, we have to determine what is the minimum time required to rot all oranges. A rotten orange at index [i,j] can rot other fresh
+ * orange at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) in unit time. If it is impossible to rot every orange then simply return -1.
+ */
+
+/**
+ * Sample Input
+ * 2
+ * 3 5
+ * 2 1 0 2 1 1 0 1 2 1 1 0 0 2 1
+ * 3 5
+ * 2 1 0 2 1 0 0 1 2 1 1 0 0 2 1
+ */
+
 public class RottenOranges {
 
     public static void main(String[] args) throws IOException {
