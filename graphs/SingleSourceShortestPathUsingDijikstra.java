4d11329826a734e3bf91a6f19bf76980a66a7f59
Aayush Srivastava <aayush03@users.noreply.github.com>
2020-06-09 03:06:03 +0530

    Single Source Shortest Path Using Dijikstra

---
+++ graphs/SingleSourceShortestPathUsingDijikstra.java
@@ -0,0 +1,109 @@
+package graphs;
+
+/**
+ * @author Aayush Srivastava
+ */
+public class SingleSourceShortestPathUsingDijikstra extends WeightedGraph {
+
+    public SingleSourceShortestPathUsingDijikstra(int noOfVertices) {
+        super(noOfVertices);
+    }
+
+    public static void main(String[] args) {
+        SingleSourceShortestPathUsingDijikstra obj1 = new SingleSourceShortestPathUsingDijikstra(5);
+        obj1.addUndirectedEdge(0, 1, 9);
+        obj1.addUndirectedEdge(0, 2, 6);
+        obj1.addUndirectedEdge(0, 3, 5);
+        obj1.addUndirectedEdge(0, 4, 3);
+
+        obj1.addUndirectedEdge(2, 1, 2);
+        obj1.addUndirectedEdge(2, 3, 4);
+
+        obj1.dijikstra(0);
+
+        System.out.println();
+
+        int adjacencyMatrix[][] = new int[][]
+                {
+                        {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                        {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                        {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                        {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                        {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                        {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                        {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                };
+
+        SingleSourceShortestPathUsingDijikstra obj2 = new SingleSourceShortestPathUsingDijikstra(9);
+        obj2.dijikstra(adjacencyMatrix, 0);
+    }
+
+    private void dijikstra(int src) {
+        minHeap.add(new GraphNode(src, 0));
+        distance[src] = 0;
+
+        while (!minHeap.isEmpty()) {
+            GraphNode u = minHeap.poll();
+            for (GraphNode v : adjacencyList.get(u.node)) {
+                relaxEdge(u, v);
+            }
+        }
+
+        printSolution(src);
+    }
+
+    private void dijikstra(int[][] graph, int src) {
+        minHeap.add(new GraphNode(src, 0));
+        distance[src] = 0;
+
+        while (!minHeap.isEmpty()) {
+            GraphNode u = minHeap.poll();
+            for (int v = 0; v < graph[u.node].length; v++) {
+                if (graph[u.node][v] != 0)
+                    relaxEdge(graph, u.node, v);
+            }
+        }
+
+        printSolution(src);
+    }
+
+    private void relaxEdge(int[][] graph, int i, int j) {
+        if (distance[j] > distance[i] + graph[i][j]) {
+            distance[j] = distance[i] + graph[i][j];
+            parents[j] = i;//tracking only needed to retrace the path later
+            minHeap.add(new GraphNode(j, distance[j]));
+        }
+    }
+
+    private void relaxEdge(GraphNode u, GraphNode v) {
+        if (distance[v.node] > distance[u.node] + v.weight) {
+            distance[v.node] = distance[u.node] + v.weight;
+            parents[v.node] = u.node;//tracking only needed to retrace the path later
+            minHeap.add(new GraphNode(v.node, distance[v.node]));
+        }
+    }
+
+    private void printSolution(int startVertex) {
+        System.out.print("Vertex\t Distance\tPath");
+        for (int vertexIndex = 0; vertexIndex < noOfVertices; vertexIndex++) {
+            if (vertexIndex != startVertex) {
+                System.out.print("\n" + startVertex + " -> ");
+                System.out.print(vertexIndex + " \t\t ");
+                System.out.print(distance[vertexIndex] + "\t\t");
+                printPath(vertexIndex);
+            }
+        }
+    }
+
+    private void printPath(int currentVertex) {
+        if (currentVertex == NO_PARENT)
+            return;
+
+        printPath(parents[currentVertex]);
+        System.out.print(currentVertex + " ");
+    }
+
+}
+
---
+++ graphs/WeightedGraph.java
@@ -0,0 +1,51 @@
+package graphs;
+
+import java.util.*;
+
+/**
+ * @author Aayush Srivastava
+ */
+public class WeightedGraph {
+
+    protected final int NO_PARENT = -1;
+    protected List<List<GraphNode>> adjacencyList;
+    protected int noOfVertices;
+    protected int[] distance;
+    protected Queue<GraphNode> minHeap;
+    protected int[] parents;
+
+    public WeightedGraph(int noOfVertices) {
+        this.noOfVertices = noOfVertices;
+        this.adjacencyList = new ArrayList<>(noOfVertices);
+        for (int i = 0; i < noOfVertices; i++)
+            adjacencyList.add(new ArrayList<>());
+
+        this.distance = new int[noOfVertices];
+        for (int i = 0; i < noOfVertices; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+        this.minHeap = new PriorityQueue<>(noOfVertices, (a, b) -> a.weight - b.weight);
+        this.parents = new int[noOfVertices];
+        Arrays.fill(parents, NO_PARENT);
+    }
+
+    protected void addDirectedEdge(int sourceVertex, int destinationVertex, int costToReachDestinationVertex) {
+        adjacencyList.get(sourceVertex).add(new GraphNode(destinationVertex, costToReachDestinationVertex));
+    }
+
+    protected void addUndirectedEdge(int sourceVertex, int destinationVertex, int costToReachDestinationVertex) {
+        adjacencyList.get(sourceVertex).add(new GraphNode(destinationVertex, costToReachDestinationVertex));
+        adjacencyList.get(destinationVertex).add(new GraphNode(sourceVertex, costToReachDestinationVertex));
+    }
+}
+
+class GraphNode {
+    public int node;
+    public int weight;
+
+    public GraphNode(int node, int weight) {
+        this.node = node;
+        this.weight = weight;
+    }
+
+}
