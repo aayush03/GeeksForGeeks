e0078e91887b45d4923efc6c428c5ee205cd9daf
Aayush Srivastava <aayush03@users.noreply.github.com>
2020-07-13 19:31:20 +0530

    dijikstra

---
+++ graphs/dijikstra/NetworkDelayTime.java
@@ -0,0 +1,63 @@
+package graphs.dijikstra;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @author Aayush Srivastava
+ */
+
+/**
+ * There are N network nodes, labelled 1 to N.
+ * <p>
+ * Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node,
+ * v is the target node, and w is the time it takes for a signal to travel from source to target.
+ * <p>
+ * Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+ * <p>
+ * Sample Input: times = [[2,1,1],[2,3,1],[3,4,1]], N = 4, K = 2
+ * Output: 2
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * 1.) N will be in the range [1, 100].
+ * 2.) K will be in the range [1, N].
+ * 3.) The length of times will be in the range [1, 6000].
+ * 4.) All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 0 <= w <= 100.
+ */
+public class NetworkDelayTime {
+
+    public int networkDelayTime_Dijkstra(int[][] times, int N, int K) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge : times) {
+            graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        boolean[] visited = new boolean[N + 1];
+        int[] minDis = new int[N + 1];
+        Arrays.fill(minDis, Integer.MAX_VALUE);
+        minDis[K] = 0;
+        pq.offer(new int[]{0, K});
+        int max = 0;
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int currNode = curr[1];
+            if (visited[currNode]) continue;
+            visited[currNode] = true;
+            int currDis = curr[0];
+            max = currDis;
+            N--;
+            if (!graph.containsKey(currNode)) continue;
+            for (int[] next : graph.get(currNode)) {
+                if (!visited[next[0]] && currDis + next[1] < minDis[next[0]])
+                    pq.offer(new int[]{currDis + next[1], next[0]});
+            }
+        }
+        return N == 0 ? max : -1;
+    }
+}
--- graphs/dijikstra/WeightedGraph.java
+++ graphs/dijikstra/WeightedGraph.java
@@ -1,4 +1,4 @@
-package graphs;
+package graphs.dijikstra;
 
 import java.util.*;
 
