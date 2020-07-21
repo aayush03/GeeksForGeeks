a31f7e798a89c96ca68c40c20919dd77946a4ff2
Aayush Srivastava <aayush03@users.noreply.github.com>
2020-07-21 05:33:49 +0530

    Updated GenerateParentheses description

--- backtracking/GenerateParentheses.java
+++ backtracking/GenerateParentheses.java
@@ -3,6 +3,18 @@ package backtracking;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * <p>
+ * For example, given n = 3, a solution set is:
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ */
 public class GenerateParentheses {
 
     public static void main(String[] args) {
